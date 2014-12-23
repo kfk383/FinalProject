1f1135e4d03cecd870aa10311aa2ad6e8f9b3ffa
kfk383 <kfk383@users.noreply.github.com>
2014-12-23 13:14:43 -0500

    Worked on GUI interface, set background image

--- FinalProject.java
+++ FinalProject.java
@@ -1,14 +1,48 @@
-import java.
-public class FinalProject extends JPanel{
- private Image backgroundImage;
-  public JPanelWithBackground(String fileName) throws IOException {
-    backgroundImage = ImageIO.read(new File(  ));
-  }
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.awt.image.BufferedImage.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.lang.Object;
+import javax.imageio.ImageIO;
+import java.io.IOException;
 
-  public void paintComponent(Graphics g) {
-    super.paintComponent(g);
-   g.drawImage(backgroundImage, 0, 0, this);
-  }
+public class FinalProject extends JFrame implements ActionListener{
+    private Container pane;
+    private JButton b,clear;
+    private JLabel l;
+    private JTextField text;
+    private Container buttons;
+    private Container textyStuff;
+    private Image backgroundImage;
+
+    public FinalProject(){
+	this.setTitle("2D Bowling -- Fall 2014 APCS Final Project");
+	this.setSize(1000,1000);
+	this.setLocation(100,100);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public void JPanelWithBackground(String fileName) throws IOException{
+	String path = ".//mainmenu.jpg";
+	File mainmenu = new File(path);
+	backgroundImage = ImageIO.read(mainmenu);
+    }
+
+    public void actionPerformed(ActionEvent e){
+	String action = e.getActionCommand();
+    }
+
+    public void paintComponent(Graphics g) {
+	paintComponent(g);
+	g.drawImage(backgroundImage, 0, 0, this);
+    }
+
+    public static void main(String[]args){
+	FinalProject a = new FinalProject();
+    }
 
 
 
