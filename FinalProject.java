55fd8e4680edab1f993520b31edd0403f59149ce
Weihwu <weihwu@yahoo.com>
2015-01-06 13:08:35 -0500

    Made Start Window

--- FinalProject.java
+++ FinalProject.java
@@ -1,58 +1,34 @@
-import javax.swing.*;
 import java.awt.event.*;
+import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage.*;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.*;
-import java.lang.Object;
+import java.io.*;
+import java.util.logging.*;
 import javax.imageio.ImageIO;
-import java.io.IOException;
 
-public class FinalProject extends JFrame implements ActionListener{
+public class StartWindow extends JFrame implements ActionListener{
     private Container pane;
-    private JButton b,clear;
-    private JLabel l;
-    private JTextField text;
-    private Container buttons;
-    private Container textyStuff;
-    private Image backgroundImage;
+    private JLabel title;
 
-    public FinalProject() throws IOException{
-	this.setTitle("2D Bowling -- Fall 2014 APCS Final Project");
-	this.setSize(1000,1000);
+    public StartWindow(){
+	this.setTitle("Lucky Strike");
+	this.setSize(500,1000);
 	this.setLocation(100,100);
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-	this.JPanelWithBackground(".//mainmenu.jpg");
-    }
+	
+	pane = this.getContentPane();
+	pane.setLayout(new FlowLayout());
 
-    public void JPanelWithBackground(String fileName) throws IOException{
-	File f = new File(fileName);
-	backgroundImage = ImageIO.read(f);
+	title = new ImageIcon("Name.png");
+	pane.add(title);
     }
-
+    
     public void actionPerformed(ActionEvent e){
-	String action = e.getActionCommand();
     }
-
-    public void paint(Graphics g) {
-	g.drawImage(backgroundImage, 0, 0, this);
+ 
+    public static void main(String[] args){
+	StartWindow a = new StartWindow();
+	a.setVisible(true);
     }
-
-    public static void main(String[]args){
-	try{
-	    FinalProject a = new FinalProject();
-	    Graphics g = a.getGraphics();
-	    a.JPanelWithBackground("mainmenu.jpg");
-	    a.paint(g);
-	    a.setVisible(true);
-	}
-	catch (IOException e){
-	    e.printStackTrace();
-	}
-    }
-
-
-
 }
 
+    
