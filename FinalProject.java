48f24c35e60390bbaef02b33fb2c82164e1634cd
Weihwu <weihwu@yahoo.com>
2015-01-07 13:01:18 -0500

    Updated README and background image

--- FinalProject.java
+++ FinalProject.java
@@ -5,52 +5,42 @@ import java.io.*;
 import java.util.logging.*;
 import javax.imageio.ImageIO;
 
-public class FinalProject extends JFrame implements ActionListener{
-    private Container pane;
-    private JLabel title;
-
-    public StartWindow(){
-	this.setTitle("Lucky Strike");
-	this.setSize(700,900);
-	this.setLocation(100,100);
-	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-	
-	pane = this.getContentPane();
-	pane.setLayout(new FlowLayout());
-    }
-
-    public void JPanelWithBackground(String fileName) throws IOException{
-	File f = new File(fileName);
-	backgroundImage = ImageIO.read(f);
-	title = new ImageIcon("mainmenu.jpg");
-	pane.add(title);
-    }
+public class FinalProject{
     
-    public void actionPerformed(ActionEvent e){
-	String action = e.getActionCommand();
-    }
-
-    public void paint(Graphics g) {
-	g.drawImage(backgroundImage, 0, 0, this);
-    }
-
-    public static void main(String[]args){
-	try{
-	    FinalProject a = new FinalProject();
-	    Graphics g = a.getGraphics();
-	    a.JPanelWithBackground("mainmenu.jpg");
-	    a.paint(g);
-	    a.setVisible(true);
-	}
-	catch (IOException e){
-	    e.printStackTrace();
-	}
-    }
- 
     public static void main(String[] args){
-	FinalProject a = new FinalProject();
-	a.setVisible(true);
+	JFrame frame;
+	ImagePanel background;
+	
+	frame = new JFrame();
+	background = new ImagePanel(new ImageIcon("mainmenu.jpg").getImage());
+
+	frame.getContentPane().add(background);
+	frame.pack();
+	frame.setVisible(true);
     }
 }
 
+class ImagePanel extends JPanel{
     
+    private Image img;
+    
+    public ImagePanel(String img){
+	this(new ImageIcon(img).getImage());
+    }
+
+    public ImagePanel(Image img){
+	this.img = img;
+	Dimension size = new Dimension(img.getWidth(null), img.getHeight(null));
+	setPreferredSize(size);
+	setMinimumSize(size);
+	setMaximumSize(size);
+	setSize(size);
+	setLayout(null);
+    }
+
+    public void paintComponent(Graphics g){
+	g.drawImage(img, 0, 0, null);
+    }
+}
+	
+	
