7b64d644742a26fe86a4e73370f0b13f2a65a01e
kfk383 <kristifok383@gmail.com>
2015-01-07 19:05:33 -0500

    Added in java documentation descriptions except for the mouseListener code

--- FinalProject.java
+++ FinalProject.java
@@ -6,7 +6,9 @@ import java.util.logging.*;
 import javax.imageio.ImageIO;
 
 public class FinalProject{
-    
+    /**
+     *Main function to set up GUI and allow the user to interact with the program
+     */
     public static void main(String[] args){
 	JFrame frame;
 	ImagePanel background;
@@ -23,11 +25,19 @@ public class FinalProject{
 class ImagePanel extends JPanel implements ActionListener{
     
     private Image img;
-    
+
+    /**
+     *Converts the image into an icon that can be added into the GUI container.
+     *@param img the file path of the image that will be converted into an icon.
+     */    
     public ImagePanel(String img){
 	this(new ImageIcon(img).getImage());
     }
 
+    /**
+     *Takes the dimensions of an image and constructs a container with those dimensions.
+     *@param img the image whose dimensions will be used to set the size of the container.
+     */
     public ImagePanel(Image img){
 	this.img = img;
 	Dimension size = new Dimension(img.getWidth(null), img.getHeight(null));
@@ -38,22 +48,35 @@ class ImagePanel extends JPanel implements ActionListener{
 	setLayout(null);
     }
 
+    /**
+     *Override of the built in paintComponent. This is where the painting code that acts on the container is used.
+     *@param g location in the container in which the image is intended to be drawn.
+     */
     public void paintComponent(Graphics g){
 	g.drawImage(img, 0, 0, null);
     }
 
+    /**
+     *Takes actions that have been performed in the GUI and translates it to call on the code for the game.
+     *@param e the commands that are coming in from the GUI.
+     */
     public void actionPerformed(ActionEvent e){
 	String action = e.getActionCommand();
     }
 
     class CustomMouseListener implements MouseListener{
 	public void mouseClicked(MouseEvent e){
+	    
 	}
 	public void mousePressed(MouseEvent e){
+	    int x = getX();
+	    int y = getY();
 	}
 	public void mouseReleased(MouseEvent e){
 	}
 	public void mouseEntered(MouseEvent e){
+	    int x = getX();
+	    int y = getY();
 	}
 	public void mouseExited(MouseEvent e){
 	}
